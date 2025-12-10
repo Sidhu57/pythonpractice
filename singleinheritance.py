b97db6e12a14e7d82f5944f2fa28e3c3cb83b5c9
Santhana Bharathi A <as7842501@gmail.com>
2025-12-10 12:29:15 +0530

    created a new file loop.py and singleinheritance.py

---
+++ singleinheritance.py
@@ -0,0 +1,10 @@
+class sidhu():
+    def money(self):
+        print("money")
+class shelin(sidhu):
+    def rent(self):
+        print("rent")
+k1 = shelin()
+k1.rent()
+k1.money()
+#single inheritance hence i can acesss the class sidhu with the class shelin 
