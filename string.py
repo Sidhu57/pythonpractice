0d3fb2f11a50d6e23e40f6d255edb0a3a5a1a1b4
Santhana Bharathi A <as7842501@gmail.com>
2025-12-10 15:18:40 +0530

    modified the string.py

--- string.py
+++ string.py
@@ -1,3 +1,4 @@
+# checking if the string contains all the words in the chars
 string = "santhanabharathi"
 chars = ['a', 'e', 'u']
 
@@ -10,3 +11,18 @@ if yes_itcontain:
     print ("yes, it  has all the words which contains in the list")
 else:
     print("no the string doesnot have all the characters from the list")
+#replace every vowel in the string with the next item in the list
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+string = "santhanabharathi"
+result = ""
+
+for ch in string:
+    if ch in vowels:
+        idx = vowels.index(ch)
+        new_idx = (idx + 1) % len(vowels)   # move to next vowel (wrap around)
+        result += vowels[new_idx]
+    else:
+        result += ch
+
+print(result)
