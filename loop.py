7c0c31f763a7d939d168f93bd6d8ffcda0d4e912
Santhana Bharathi A <as7842501@gmail.com>
2025-12-10 12:34:23 +0530

    modified loop.py

--- loop.py
+++ loop.py
@@ -1,1 +1,19 @@
-print("hello")
+limit = 5  
+
+print("Using for loop:")
+for number in range(1, 6):  
+    print(f"Multiples of {number}: ", end="")
+    for multiple in range(1, limit + 1):
+        print(number * multiple, end=" ")
+    print()  
+
+print("\nUsing while loop:")
+number = 1
+while number <= 5:
+    print(f"Multiples of {number}: ", end="")
+    multiple = 1
+    while multiple <= limit:
+        print(number * multiple, end=" ")
+        multiple += 1
+    print()  
+    number += 1
